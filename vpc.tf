55b5a85a62bf017f1f604298e859057639982969
pratik-databricks <82140743+pratik-databricks@users.noreply.github.com>
2022-01-22 16:58:59 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.73.0"
+    }
+  }
+}
+
 resource "aws_vpc" "main" {
   cidr_block       = "10.134.0.0/16"
   instance_tenancy = "default"
